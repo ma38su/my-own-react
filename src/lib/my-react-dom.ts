2524e81c73b58c50118b188f2117da3405eeb22b
ma38su <ma38su@gmail.com>
2023-01-04 14:38:31 +0900

    step2, step3

--- src/lib/my-react-dom.ts
+++ src/lib/my-react-dom.ts
@@ -1,29 +1,102 @@
-import { MyReactElement } from "./my-react.js";
+type MyReactFiber = {
+  type: string,
+  dom: HTMLElement | Text | null,
+  props: {
+    [key: string]: any,
+    children: MyReactFiber[],
+  },
+  parent: MyReactFiber | null,
+  child: MyReactFiber | null,
+  sibling: MyReactFiber | null,
+};
 
-function render(element: MyReactElement, container: HTMLElement) {
-    const { type } = element;
+function render(fiber: MyReactFiber, container: HTMLElement) {
+  nextUnitOfWork = {
+    type: fiber.type,
+    dom: container,
+    props: {
+      children: [fiber],
+    },
+    parent: null,
+    child: null,
+    sibling: null,
+  }
+}
 
-    if (type === "TEXT_ELEMENT") {
-        const dom = document.createTextNode("");
-        const isProperty = (key: string) => key !== 'children';
-        Object.keys(element.props).filter(isProperty).forEach(name => {
-            (dom as any)[name] = element.props[name];
-        })
-        container.appendChild(dom);
+// 無限ループ
+let nextUnitOfWork: MyReactFiber | null = null;
+function workLoop(deadline: IdleDeadline) {
+  let shouldYield = false;
+  while (nextUnitOfWork && !shouldYield) {
+    nextUnitOfWork = performUnitOfWork(nextUnitOfWork);
+    shouldYield = deadline.timeRemaining() < 1;
+  }
+  requestIdleCallback(workLoop);
+}
+requestIdleCallback(workLoop);
+
+function createDom(fiber: MyReactFiber): HTMLElement | Text {
+  const { type } = fiber;
+  const dom = type === "TEXT_ELEMENT" ? document.createTextNode("") : document.createElement(type);
+
+  const isProperty = (key: string) => key !== 'children';
+  Object.keys(fiber.props)
+    .filter(isProperty)
+    .forEach(name => {
+      (dom as any)[name] = fiber.props[name];
+    })
+  return dom;
+}
+
+function performUnitOfWork(fiber: MyReactFiber): MyReactFiber | null {
+
+  // add dom node
+  if (!fiber.dom) {
+    fiber.dom = createDom(fiber);
+  }
+
+  if (fiber.parent?.dom) {
+    fiber.parent.dom.appendChild(fiber.dom);
+  }
+
+  // create new fibers
+  const elements = fiber.props.children;
+  let index = 0;
+  let prevSibling: MyReactFiber | null = null;
+  while (index < elements.length) {
+    const element = elements[index];
+    const newFiber = {
+      type: element.type,
+      dom: null,
+      props: element.props,
+      parent: fiber,
+      child: null,
+      sibling: null,
+    };
+
+    if (index === 0) {
+      fiber.child = newFiber;
     } else {
+      if (prevSibling == null) throw new Error('assert');
 
-        const dom = document.createElement(type);
-        element.props.children.forEach((child) => {
-            render(child, dom);
-        });
-
-        const isProperty = (key: string) => key !== 'children';
-        Object.keys(element.props).filter(isProperty).forEach(name => {
-            (dom as any)[name] = element.props[name];
-        })
-
-        container.appendChild(dom);
+      prevSibling.sibling = newFiber;
     }
+    prevSibling = newFiber;
+    index += 1;
+  }
+
+  // return next unit of work
+  if (fiber.child) {
+    return fiber.child;
+  }
+  let nextFiber: MyReactFiber | null = fiber;
+  while (nextFiber) {
+    if (nextFiber.sibling) {
+      return nextFiber.sibling;
+    }
+    nextFiber = nextFiber.parent;
+  }
+  return null;
 }
 
 export { render };
